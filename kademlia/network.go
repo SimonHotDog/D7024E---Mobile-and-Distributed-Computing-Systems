eefa416f96e3f9e5e0ea4ef2923d70bd17a55ab3
klaman-8 <klaman-8@student.ltu.se>
2022-09-10 21:32:08 +0200

    Implemented ping in go + enabled go on image

--- kademlia/network.go
+++ kademlia/network.go
@@ -1,24 +1,53 @@
 package kademlia
 
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/go-ping/ping"
+)
+
 type Network struct {
 }
 
-func Listen(ip string, port int) {
-	// TODO
-}
+func SendPingMessage(ip string) {
+	pinger, err := ping.NewPinger(ip)
+	pinger.SetPrivileged(true)
+	if err != nil {
+		panic(err)
+	}
 
-func (network *Network) SendPingMessage(contact *Contact) {
-	// TODO
-}
+	// Listen for Ctrl-C.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for _ = range c {
+			pinger.Stop()
+		}
+	}()
 
-func (network *Network) SendFindContactMessage(contact *Contact) {
-	// TODO
-}
+	pinger.OnRecv = func(pkt *ping.Packet) {
+		fmt.Printf("%d bytes from %s: icmp_seq=%d time=%v\n",
+			pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt)
+	}
 
-func (network *Network) SendFindDataMessage(hash string) {
-	// TODO
-}
+	pinger.OnDuplicateRecv = func(pkt *ping.Packet) {
+		fmt.Printf("%d bytes from %s: icmp_seq=%d time=%v ttl=%v (DUP!)\n",
+			pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt, pkt.Ttl)
+	}
 
-func (network *Network) SendStoreMessage(data []byte) {
-	// TODO
+	pinger.OnFinish = func(stats *ping.Statistics) {
+		fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
+		fmt.Printf("%d packets transmitted, %d packets received, %v%% packet loss\n",
+			stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
+		fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
+			stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
+	}
+
+	fmt.Printf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
+	err = pinger.Run()
+	if err != nil {
+		panic(err)
+	}
 }
