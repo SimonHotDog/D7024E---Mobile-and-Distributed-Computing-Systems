457487a869fca573f84ba5c19d77f8efe435552f
Rickos99 <12218241+Rickos99@users.noreply.github.com>
2022-10-02 18:33:08 +0200

    Delete code coverage of testing utilities

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 go test -coverpkg=./... -coverprofile=coverage.out ./...
+
+# Remove all internal test utilities that should not be tested
+sed -i "/d7024e\/internal\/test/d" "coverage.out"
+
+# Generate HTML report and display total root package code coverage
 go tool cover -html=coverage.out -o coverage.html
 go tool cover -func coverage.out | grep total
